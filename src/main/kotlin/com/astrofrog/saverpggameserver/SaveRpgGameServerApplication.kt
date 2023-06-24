3ca2e4c4aa482ddd280ad7596fe574e8f44a67de
ImOhashi <leonardoohashi.mkt@gmail.com>
2023-06-24 15:51:44 -0300

    first commit

---
+++ src/main/kotlin/com/astrofrog/saverpggameserver/SaveRpgGameServerApplication.kt
@@ -0,0 +1,11 @@
+package com.astrofrog.saverpggameserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SaveRpgGameServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<SaveRpgGameServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/astrofrog/saverpggameserver/SaveRpgGameServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.astrofrog.saverpggameserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SaveRpgGameServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
